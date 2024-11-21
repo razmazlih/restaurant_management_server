635bc5de949e03ddfa378c90b37e8516a95c3ac9
Raz <retraz123@gmail.com>
2024-11-21 16:08:26 +0200

    change the auth to JWT

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -81,8 +81,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
